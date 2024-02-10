7107cc8604a4bf81782c337f2075ae3ba38cb356
Natan Dourado <nsdourado12@gmail.com>
2024-02-10 12:24:56 -0300

    upload

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// Sombrinha na Navbar quando scrolla
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () =>{
+  header.classList.toggle('shadow', window.scrollY > 0);
+});
